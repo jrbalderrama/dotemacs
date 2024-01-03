2addc84f5810355ea76bcbb41e1d83fb00919f8c
jrbalderrama <jrbalderrama@users.noreply.github.com>
2024-01-04 00:02:16 +0100

    update prog modes with use-package

--- elisp/init-prog.el
+++ elisp/init-prog.el
@@ -6,66 +6,63 @@
 ;;   :config
 ;;   (add-to-list 'aggressive-indent-excluded-modes 'yaml-mode))
 
+(use-package company
+  :hook (prog-mode . company-mode))
 
-(use-package company)
-
-(use-package diff-hl)
+(use-package diff-hl
+  ;; :config (turn-on-diff-hl-mode)
+  :hook ((prog-mode . turn-on-diff-hl-mode)
+         (vc-dir-mode . turn-on-diff-hl-mode)))
 
 (use-package flycheck
-  :config
-  ;; override pop-up font settings
-  (add-to-list 'face-remapping-alist
-               '(popup-tip-face :height 0.7
-                                :box t
-                                :background "#D0BF8F"
-                                :foreground "#2B2B2B"))
-  :bind
-  (:map flycheck-command-map
-        ;; use 'C-c ! t' to with tip cycle
-        ("t" . #'flycheck-tip-cycle)))
+  :hook (prog-mode . flycheck-mode)
+  :bind (:map flycheck-command-map
+              ;; use 'C-c ! t' to with tip cycle
+              ("t" . #'flycheck-tip-cycle)))
 
-(use-package flycheck-tip
+(use-package flycheck-pos-tip
   :after flycheck
-  :config
-  (setq
-   ;; function to display on error messages
-   flycheck-display-errors-function
-   'flycheck-tip-display-current-line-error-message
-   ;; disable error messages at point in mini-buffer
-   flycheck-display-errors-function nil)
-  ;; use pop-up tool-tips instead of echo area
-  :hook flycheck-tip-cycle)
-
+  :config (flycheck-pos-tip-mode)
+  :custom
+  ;; TODO 'face-remap-add-relative
+  (pos-tip-foreground-color "white")
+  (pos-tip-internal-border-width 10)
+  (pos-tip-background-color "black"))
 
 (use-package jinja2-mode
+  :disabled
+  :defer t
   :mode "\\.j2\\'")
 
 (use-package lua-mode
+  :disabled
+  :defer t
   :mode "\\.lua\\'")
 
 (use-package yaml-mode
+  :disabled
+  :defer t
   :mode "\\.ya?ml$"
   :bind
   (:map yaml-mode-map
         ("C-m" . newline-and-indent)))
 
 (with-eval-after-load "prog-mode"
-
   (setq
    ;; disable ligatures at point
    prettify-symbols-unprettify-at-point t
-   ;; events to activate whitespace style
+   ;; events to activate white space style
    whitespace-style '(face lines-tail space-before-tab
                            indentation space-after-tab))
+
   ;; change display-line-numbers-mode face attributes
   (add-to-list 'face-remapping-alist
                '(line-number
-                 :inherit (shadow default)
-                 :height 0.6))
+                 ;;:inherit (shadow default)
+                 :height 0.75))
+
   ;; (set-face-attribute 'line-number nil :xxx)
 
-  ;; enable flycheck for programming
-  (add-hook 'prog-mode-hook 'flycheck-mode)
   ;; show long lines in different style
   (add-hook 'prog-mode-hook #'whitespace-mode)
   ;; spell only strings not source code
@@ -73,9 +70,8 @@
   ;; show line numbers
   (add-hook 'prog-mode-hook #'display-line-numbers-mode)
   ;; highlights uncommitted changes next to line number
-  (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
-  (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
-  (add-hook 'prog-mode-hook 'diff-hl-flydiff-mode))
+  ;; (add-hook 'prog-mode-hook 'diff-hl-flydiff-mode)
+  )
 
 ;; (load "~/.emacs.d/elisp/init-elisp.el")
 ;; (require 'init-python)
