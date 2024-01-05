635431e9b8e73048ed03c62a788110b7d3f7ae48
jrbalderrama <jrbalderrama@users.noreply.github.com>
2024-01-05 13:52:41 +0100

    setup a simple earyl init

--- early-init.el
+++ early-init.el
@@ -1,12 +1,17 @@
 ;;; early-init.el --- Emacs init configuration -*- lexical-binding: t -*-
 ;;
 ;;; Commentary:
+;;  - maximize initial frame on startup
+;;  - remove useless decoration
+;;  - remove message 'Package cl is deprecated'
+
 ;;
 ;;; Code:
+(push '(fullscreen . fullboth) initial-frame-alist)
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
-
+(setq byte-compile-warnings '(cl-functions))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; coding: utf-8
