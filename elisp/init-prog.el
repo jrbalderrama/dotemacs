2ec5389ee8147410c2daa1ca7b79ccb2ce5aace7
jrbalderrama <jrbalderrama@users.noreply.github.com>
2024-01-05 21:44:40 +0100

    base configuration for python development

--- elisp/init-prog.el
+++ elisp/init-prog.el
@@ -6,13 +6,54 @@
 ;;   :config
 ;;   (add-to-list 'aggressive-indent-excluded-modes 'yaml-mode))
 
+;; TODO
+;; - ein
+;; ruff linter
+;; refactoring
+;; lsp-ui
+;; pydoc edoc
+;; pytest
+;; Vertico and Corfu and Orderless + (embark, consult)
 (use-package company
-  :hook (prog-mode . company-mode))
+  :diminish
+  :hook (prog-mode . company-mode)
+  :bind (:map company-active-map
+              ("<tab>" . company-complete-selection))
+  :custom
+  (company-idle-delay 0)
+  (company-minimum-prefix-length 2))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
+
+(use-package company-quickhelp
+  :hook (company-mode . company-quickhelp-mode))
+
+(use-package dap-mode
+  :disabled
+  :after lsp-mode
+  :config (dap-auto-configure-mode))
+
+(use-package exec-path-from-shell
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
 
 (use-package diff-hl
   :config (diff-hl-flydiff-mode)
   :hook ((prog-mode . turn-on-diff-hl-mode)
          (vc-dir-mode . turn-on-diff-hl-mode)))
+
+(use-package direnv
+  :after exec-path-from-shell
+  :hook
+  (prog-mode . direnv-mode))
+
+;; use direnv instead
+(use-package envrc
+  :disabled
+  :after exec-path-from-shell
+  :config (envrc-global-mode))
 
 (use-package flycheck
   :hook (prog-mode . flycheck-mode)
@@ -28,6 +69,100 @@
   (pos-tip-foreground-color "white")
   (pos-tip-internal-border-width 10)
   (pos-tip-background-color "black"))
+
+(use-package flycheck-pycheckers
+  :after flycheck
+  :custom (flycheck-pycheckers-checkers '(mypy3 flake8))
+  ;; :config
+  ;; (setq flycheck-pycheckers-ignore-codes (append flycheck-pycheckers-ignore-codes '("E0731" "E0741")))
+  :hook (flycheck-mode-hook . flycheck-pycheckers-setup))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :init (setq lsp-keymap-prefix "C-c l")
+  :bind (:map lsp-mode-map
+        ("<tab>" . company-indent-or-complete-common))
+  :hook
+  (python-mode . lsp-deferred)
+  (lsp-mode . lsp-enable-which-key-integration))
+
+(use-package lsp-pyright
+  :after lsp-mode
+  :custom
+  (lsp-pyright-disable-language-service nil)
+  (lsp-pyright-disable-organize-imports t))
+
+(use-package lsp-treemacs
+  :after lsp-mode)
+
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-sideline-enable t)
+  (lsp-ui-sideline-show-hover nil)
+  (lsp-ui-sideline-show-diagnostics nil)
+  (lsp-ui-sideline-show-code-actions t)
+  (lsp-ui-doc-delay 2)
+  :hook
+  (lsp-mode . lsp-ui-mode)
+  :bind (:map lsp-ui-mode-map
+              ("C-c i" . lsp-ui-imenu)))
+
+(use-package reformatter
+  :hook (python-mode . darker-reformat-on-save-mode)
+  :config
+  (reformatter-define darker-reformat
+    :program "darker"
+    :stdin nil
+    :stdout nil
+    :args (list "-q" "-i" input-file)))
+
+(use-package pyenv-mode
+  :disabled
+  :init (setq exec-path (append exec-path '("~/.pyenv/bin")))
+  :hook (python-mode . pyenv-mode))
+
+(use-package python-pytest
+  :disabled)
+
+(use-package python
+  :custom
+  ;; Remove guess indent python message
+  (python-indent-guess-indent-offset-verbose nil)
+  :config
+  ;; Use IPython when available or fall back to regular Python
+  (cond
+   ((executable-find "ipython")
+    (progn
+      (setq python-shell-buffer-name "IPython")
+      (setq python-shell-interpreter "ipython")
+      (setq python-shell-interpreter-args "-i --simple-prompt")))
+   ((executable-find "python3")
+    (setq python-shell-interpreter "python3"))
+   ((executable-find "python2")
+    (setq python-shell-interpreter "python2"))
+   (t
+    (setq python-shell-interpreter "python"))))
+
+;; use direnv instead
+(use-package pyvenv
+  :disabled
+  :init
+  (setenv "WORKON_HOME" "~/.pyenv/versions"))
+
+(use-package treemacs
+  :defer t
+  :custom
+  ;(treemacs-no-png-images t)
+  (treemacs-width 24)
+  :bind ("C-c t" . treemacs))
+
+(use-package which-key
+  :diminish
+  :after lsp-mode
+  :hook (lsp-mode . which-key-mode)
+  :custom
+  (which-key-sort-order 'which-key-local-then-key-order))
 
 (use-package jinja2-mode
   :disabled
@@ -74,5 +209,6 @@
 ;; (require 'init-python)
 
 (provide 'init-prog)
+
 
 ;;; init-prog.el ends here
